cd792c45288446aa027bc1bf82edb5d6c8208296
Ramon Torres <ramon.torres@edgebound.com>
2012-06-14 23:00:02 -0500

    implementing switching states (on | off) using tap

--- app/flashlight_vc.rb
+++ app/flashlight_vc.rb
@@ -1,11 +1,20 @@
 class	FlashlightViewController < UIViewController
+	attr_accessor :torch
 
 	def loadView
 		self.view = UIImageView.alloc.init
 	end
 
 	def viewDidLoad
-		self.view.image = UIImage.imageNamed( 'background.png' )
+		view.image = UIImage.imageNamed( 'background.png' )
+		view.userInteractionEnabled = true
+		tap_recognizer = UITapGestureRecognizer.alloc.initWithTarget( self, action: 'toggle_torch' )
+		view.addGestureRecognizer( tap_recognizer )
+	end
+
+	def toggle_torch
+		# change background image
+		@torch.toggle_torch
 	end
 
 end
