21ca1c69eda14e23d2175f01fd18d4ef2461e2f7
Ramon Torres <ramon.torres@edgebound.com>
2012-06-14 22:58:27 -0500

    managing application events accordingly

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,10 +1,23 @@
 class AppDelegate
 	def application( application, didFinishLaunchingWithOptions:launchOptions )
 		@torch = Torch.new
-		@torch.turn_on
 		@window = UIWindow.alloc.initWithFrame( UIScreen.mainScreen.bounds )
 		@window.rootViewController = FlashlightViewController.alloc.init
+		@window.rootViewController.torch = @torch
 		@window.makeKeyAndVisible
 		true
 	end
+
+	def applicationDidBecomeActive( application )
+		@torch.turn_on
+	end
+
+	def applicationDidEnterBackground( application )
+		@torch.turn_off
+	end
+
+	def applicationWillTerminate( application )
+		@torch.turn_off
+	end
+
 end
