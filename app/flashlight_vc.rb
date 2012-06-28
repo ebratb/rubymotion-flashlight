e51de3faa2baa9997882b18595b89d141e7f915a
Ramon Torres <ramon.torres@edgebound.com>
2012-06-27 22:48:06 -0500

    allow to show different images depending on torch's state

--- app/flashlight_vc.rb
+++ app/flashlight_vc.rb
@@ -1,19 +1,21 @@
 class	FlashlightViewController < UIViewController
 	attr_accessor :torch
 
+	BACKGORUND_IMAGES = { turned_on: 'background.png', turned_off: 'background.png', unavailable: 'background.png' }
+
 	def loadView
 		self.view = UIImageView.alloc.init
 	end
 
 	def viewDidLoad
-		view.image = UIImage.imageNamed( 'background.png' )
+		view.image = UIImage.imageNamed( BACKGORUND_IMAGES[ @torch.available? ? :turned_on : :unavailable ] )
 		view.userInteractionEnabled = true
 		tap_recognizer = UITapGestureRecognizer.alloc.initWithTarget( self, action: 'toggle_torch' )
 		view.addGestureRecognizer( tap_recognizer )
 	end
 
 	def toggle_torch
-		# change background image
+		background_image = ''
 		@torch.toggle_torch
 	end
 
